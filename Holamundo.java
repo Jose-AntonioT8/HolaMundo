ce280e6f93b862425f10a52415c780fe672fbf11
Jose <joseantdominguezgonzalez@gmail.com>
2024-10-17 14:33:00 +0200

    Update Holamundo.java

--- Holamundo.java
+++ Holamundo.java
@@ -1,11 +1,11 @@
 public class HolaMundo {
 
     public static void main(String[] args) {
-
+        holaMundo();
     }
 
     public static void holaMundo() {
-
+        System.err.println("Hola mundo");
     }
 
     public static void saludo() {
