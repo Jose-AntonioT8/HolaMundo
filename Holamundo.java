3649a27e628613e3d7c18b016413e399d4d7a2a2
Jose-AntonioT8 <jdomgon334@g.educaand.es>
2024-10-17 14:27:22 +0200

    Add files via upload

---
+++ Holamundo.java
@@ -0,0 +1,19 @@
+public class HolaMundo {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void holaMundo() {
+
+    }
+
+    public static void saludo() {
+
+    }
+
+    public static void despedida() {
+
+    }
+
+}
